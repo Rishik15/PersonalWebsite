ad47c4fd37c2944284e9684e731e151569607b57
Rishik15 <ry248@njit.edu>
2025-06-10 15:52:49 -0400

    Updated hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -111,16 +111,17 @@ export const Navbar = () => {
 
             {/* Mobile Navigation Menu */}
             <div
-            className={cn(
-                "fixed inset-0 bg-background backdrop-blur-md z-40 flex flex-col items-center justify-center",
-                "transition-all duration-300 md:hidden",
-                isMenuOpen
-                ? "opacity-100 pointer-events-auto"
-                : "opacity-0 pointer-events-none"
-            )}
-            role="menu"
-            aria-hidden={!isMenuOpen}
+                className={cn(
+                    "fixed inset-0 backdrop-blur-lg bg-background/50 border border-border shadow-lg z-40 flex flex-col items-center justify-center",
+                    "transition-all duration-300 md:hidden",
+                    isMenuOpen
+                    ? "opacity-100 pointer-events-auto"
+                    : "opacity-0 pointer-events-none"
+                )}
+                role="menu"
+                aria-hidden={!isMenuOpen}
             >
+
             <div className="flex flex-col space-y-8 text-xl">
                 {navItems.map((item, key) => (
                 <a
